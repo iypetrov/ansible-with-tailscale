cf5bd68fc755f7bbc03693d9da5611b219cc7650
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-10-17 13:44:32 +0300

    feat: add CI/CD job
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "vm_role" {
-  name = "role"
+resource "aws_iam_role" "worker_role" {
+  name = "worker-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -14,22 +14,22 @@ resource "aws_iam_role" "vm_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "vm_role_policy_attachment" {
-  role       = aws_iam_role.vm_role.name
+resource "aws_iam_role_policy_attachment" "worker_role_policy_attachment" {
+  role       = aws_iam_role.worker_role.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
-resource "aws_iam_instance_profile" "vm_profile" {
-  name = "profile"
-  role = aws_iam_role.vm_role.name
+resource "aws_iam_instance_profile" "worker_profile" {
+  name = "worker-profile"
+  role = aws_iam_role.worker_role.name
 }
 
-resource "aws_instance" "vm" {
+resource "aws_instance" "worker_one" {
   ami                         = "ami-0a628e1e89aaedf80"
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.private_subnet_a.id
-  vpc_security_group_ids      = [aws_security_group.vm_sg.id]
-  iam_instance_profile        = aws_iam_instance_profile.vm_profile.name
+  vpc_security_group_ids      = [aws_security_group.worker_sg.id]
+  iam_instance_profile        = aws_iam_instance_profile.worker_profile.name
   user_data_replace_on_change = true
   user_data                   = <<-EOF
     #!/usr/bin/env bash
@@ -37,8 +37,31 @@ resource "aws_instance" "vm" {
     apt-get update -y
     apt-get install -y curl
 
+    curl -fsSL https://tailscale.com/install.sh | sh
+    tailscale up --authkey  ${var.ts_auth_key} --hostname aws-worker-1 --ssh
+  EOF
+  tags = {
+    Name = "aws-worker-1"
+  }
+}
+
+resource "aws_instance" "worker_two" {
+  ami                         = "ami-0a628e1e89aaedf80"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.private_subnet_a.id
+  vpc_security_group_ids      = [aws_security_group.worker_sg.id]
+  iam_instance_profile        = aws_iam_instance_profile.worker_profile.name
+  user_data_replace_on_change = true
+  user_data                   = <<-EOF
+    #!/usr/bin/env bash
+
+    apt-get update -y
+    apt-get install -y curl
 
     curl -fsSL https://tailscale.com/install.sh | sh
-    tailscale up --authkey  ${var.tailscale_auth_key} --hostname "foo" --ssh
+    tailscale up --authkey  ${var.ts_auth_key} --hostname aws-worker-2 --ssh
   EOF
+  tags = {
+    Name = "aws-worker-2"
+  }
 }
